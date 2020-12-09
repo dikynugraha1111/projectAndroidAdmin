fb88cd9aa272f897804ccf9343a3c7840fbb3f34
dikynugraha1111 <diky1800018380@webmail.uad.ac.id>
2020-12-09 16:58:47 +0700

    Added image slider

--- app/src/main/java/com/example/projectandroidadmin/MainActivity.kt
+++ app/src/main/java/com/example/projectandroidadmin/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.projectandroidadmin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.projectandroidadmin.model.bannerList
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        banner_view_pager.adapter = BannerAdapter(bannerList)
+        banner_view_pager.offscreenPageLimit = 1
     }
 }
